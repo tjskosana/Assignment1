4edb1347740966582d935e56ee876cf6c0b17cad
thoko.j.skosana <thoko.j.skosana@accenture.com>
2018-07-19 07:51:48 +0200

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,7 @@ package com.company;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -15,7 +16,12 @@ public class Main {
         Scanner userinput = new Scanner(System.in);
 
         String name = "";
-        int age = 0;
+        int age;
+        int sysAge;
+        int ageDif;
+        String difference = "";
+        String OddEven = "";
+
         System.out.println("Please enter your name");
         name = userinput.nextLine();
 
@@ -32,5 +38,37 @@ else
 
         System.out.println("Please enter your age");
         age = userinput.nextInt();
+
+        Random genAge = new Random();
+        sysAge = genAge.nextInt(90-18)+18;
+        System.out.println("sysAge is  " + sysAge);
+
+        if(sysAge > age)
+        {
+            ageDif = sysAge - age;
+        }else
+            {
+            ageDif = age - sysAge;
+        }
+
+        if(age > sysAge  )
+        {
+            difference = "Older";
+        }else
+        {
+            difference = "Younger";
+        }
+        System.out.println("I am " + ">>" + sysAge + " " + "years old, which is  " + ageDif + " years " + difference + " than you." );
+
+        if(sysAge % 2 == 0)
+        {
+            OddEven = "Even";
+        }else
+        {
+            OddEven = "Odd";
+        }
+        System.out.println("Twice my age would be " + (sysAge * 2));
+        System.out.println("My age is an " + OddEven + " number");
+        System.exit(0);
     }
 }
